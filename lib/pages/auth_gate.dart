e0b5c701c6cade3716b1f6677f481b608f79368f
mtccool668 <mtccool668@gmail.com>
2024-01-14 01:07:59 -0800

    fixed login ui

--- lib/pages/auth_gate.dart
+++ lib/pages/auth_gate.dart
@@ -1,18 +1,25 @@
-import 'dart:js';
 
 import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:navtern/pages/active_page.dart';
 
-class AuthGate extends StatelessWidget {
+class AuthGate extends StatefulWidget {
+
+  AuthGate({Key? key});
+
+  @override
+  State<AuthGate> createState() => _AuthGateState();
+}
+
+class _AuthGateState extends State<AuthGate> {
   final TextEditingController nameController = TextEditingController();
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+
   bool hidden = true;
-  
-  AuthGate({Key? key});
-  
+  Icon hiddenIcon = Icon(Icons.visibility);
+
   //Welcome, please log in.
   @override
   Widget build(BuildContext context) {
@@ -21,12 +28,13 @@ class AuthGate extends StatelessWidget {
       builder: (context, snapshot) {
         if (!snapshot.hasData) {
           return Scaffold(
+            resizeToAvoidBottomInset: false,
             appBar: AppBar(title: const Text('Navtern')),
             body: Center(
               child: Column(
                 children: <Widget>[
                   Padding(
-                    padding: const EdgeInsets.fromLTRB(64, 18, 64, 18),
+                    padding: const EdgeInsets.fromLTRB(32, 5, 32, 5),
                     child: TextField(
                       controller: nameController,
                       decoration: InputDecoration(
@@ -38,7 +46,7 @@ class AuthGate extends StatelessWidget {
                     ),
                   ),
                   Padding(
-                    padding: const EdgeInsets.fromLTRB(64, 18, 64, 18),
+                    padding: const EdgeInsets.fromLTRB(32, 5, 32, 5),
                     child: TextField(
                       controller: emailController,
                       decoration: InputDecoration(
@@ -50,15 +58,24 @@ class AuthGate extends StatelessWidget {
                     ),
                   ),
                   Padding(
-                    padding: const EdgeInsets.fromLTRB(64, 18, 64, 18),
+                    padding: const EdgeInsets.fromLTRB(32, 5, 32, 5),
                     child: TextField(
                       obscureText: hidden,
                       controller: passwordController,
                       decoration: InputDecoration(
                         suffixIcon: IconButton(
-                          onPressed: () => hidden = false,
-                          icon: Icon(Icons.visibility),
-                        
+                          onPressed: () {
+                            setState(() {
+                              hidden = !hidden;
+                              if (hidden) {
+                                hiddenIcon = const Icon(Icons.visibility_off);
+                              }
+                              else {
+                                hiddenIcon = const Icon(Icons.visibility);
+                              }
+                            });
+                          },
+                          icon: hiddenIcon,
                           ),
                         border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(20),
@@ -69,13 +86,36 @@ class AuthGate extends StatelessWidget {
                   ),
                   ElevatedButton(
                     onPressed: () {
-                      //loginProfile(emailController.text, passwordController.text);
-                    
-                      emailController.clear();
-                      passwordController.clear();
+                      BuildContext currContext = context;
+
+                      loginProfile(emailController.text, passwordController.text)
+                          .then((bool success) {
+                          if (!success) {
+                            ScaffoldMessenger.of(currContext).showSnackBar(
+                              const SnackBar(
+                                content: Text("Invalid user details"),
+                              ),
+                            
+                            );
+                          }
+
+                        // Clear text fields regardless of success or failure
+                        emailController.clear();
+                        passwordController.clear();
+                      });
                     },
-                   child: const Text('Log In'),
+                    child: const Text('Log In'),
                   ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Text("Don't have a Navtern account?"),
+                  TextButton(
+                    onPressed: () {
+                      print("go to register page");
+                    },
+                    child: const Text("Sign Up")
+                  )
                 ],
               ),
             ),
@@ -87,7 +127,7 @@ class AuthGate extends StatelessWidget {
     );
   }
 
-  Future registerProfile(String name, String email, String pass) async {
+  Future<bool> registerProfile(String name, String email, String pass) async {
     try {
       await FirebaseAuth.instance.createUserWithEmailAndPassword(
         email: email,
@@ -95,20 +135,22 @@ class AuthGate extends StatelessWidget {
       );
       final user = FirebaseAuth.instance.currentUser;
       await user?.updateDisplayName(name);
+      return true;
     } catch (e) {
-      return Null;
+      return false;
     }
   }
 
-  Future loginProfile(String email, String pass) async{
+  Future<bool> loginProfile(String email, String pass) async{
     try {
       await FirebaseAuth.instance.signInWithEmailAndPassword(
         email: email,
         password: pass,
       );
+      return true;
     }
     catch (e) {
-      return Null;
+      return false;
     }
   }
 }
